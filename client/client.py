9a35fb58f46ad9bd801e22bba5280537775ff1d3
Ha Ngoc Khue <hangockhue@Has-MacBook-Pro.local>
2021-05-29 15:04:26 +0700

    update

--- client/client.py
+++ client/client.py
@@ -1,7 +1,24 @@
 import socket
 
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c.connect((socket.gethostname(), 1234))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+try:
 
-msg = c.recv(1024)
-print(msg.decode("utf-8"))
+    message = 'This is the message.  It will be repeated.'
+    print(f'sending {message}')
+    s.sendall(bytes(message, "utf-8"))
+    amount_received = 0
+    amount_expected = len(message)
+    while amount_received < amount_expected:
+        data = s.recv(16)
+        amount_received += len(data)
+        print(f'received {data}')
+
+finally:
+    s.close()
+
+# # s.sendall(b"Hello, work")
+# while True:
+#     msg = s.recv(1024)
+#
+#     print(msg.decode("utf-8"))
